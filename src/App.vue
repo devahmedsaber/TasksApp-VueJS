63ccdfa1c2ff5da989ba1f32545e33d3c52a588c
Ahmed Saber <devahmedsaber@gmail.com>
2021-05-06 06:18:34 +0200

    Tasks App With VueJs

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h3>{{ title }}</h3>
+    <Tasks :tasks="tasks" v-on:completeTaskEvent="completeTask" v-on:deleteTaskEvent="deleteTask"></Tasks>
+    <input @keyup.enter="addNewTask" type="text" class="form-control mb-2" v-model="newTask">
+    <button v-on:click="addNewTask" class="btn btn-success btn-block">Add New Task</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Tasks from '@/components/Tasks';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Tasks
+  },
+  data (){
+    return {
+      title: "Saber Tasks App",
+      newTask: null,
+      tasks: [
+        {
+          body : 'Go To Shop',
+          completed: false
+        },
+        {
+          body : 'Go To Barber',
+          completed: false
+        },
+        {
+          body : 'Go To Cinema',
+          completed: false
+        }
+      ]
+    };
+  },
+  methods: {
+    addNewTask(){
+      this.tasks.push({
+        body: this.newTask,
+        completed: false
+      });
+      this.newTask = null;
+    },
+    completeTask(task){
+      task.completed = !task.completed;
+    },
+    deleteTask(index){
+      console.log('Delete Task : ' + index);
+      this.tasks.splice(index, 1);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+
+.card {
+  padding: 20px;
+  background: #EEE;
 }
 </style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <div class="card mb-1 text-left">
+            <ul class="list-group">
+                <li class="list-group-item" v-for="(task,index) in tasks" :key="index">
+                    <div :class="task.completed ? 'completed' : ''">
+                        {{ task.body }}
+                    </div>
+                    <div class="icons">
+                        <i @click.prevent="completeTask(task)" class="fas fa-check-circle"></i>
+                        <i @click.prevent="deleteTask(index)" class="fas fa-trash-alt ml-3"></i>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        tasks: null
+    },
+    methods: {
+        completeTask(task){
+            this.$emit("completeTaskEvent", task);
+         },
+        deleteTask(index){
+            this.$emit('deleteTaskEvent', index);
+        }
+    }
+};
+</script>
+
+<style scoped>
+.completed {
+    color: green;
+    text-decoration: line-through;
+}
+
+.list-group-item {
+    display: flex;
+    justify-content: space-between;
+}
+</style>
